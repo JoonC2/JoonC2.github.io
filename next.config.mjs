55592bb8be6e084943c62a8dbeb0a3da4a2e393b
Jonathan Choi <jchoi@nbp-172-207.nbp.ruw.rutgers.edu>
2024-11-04 23:36:04 -0500

    retry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',  // Enable static export
+    basePath: '/JoonC2.github.io',  // Match your repository name
+    images: {
+      unoptimized: true,  // Disable Next.js image optimization (since it's not supported on static sites)
+  },
+};
+
 
 export default nextConfig;
